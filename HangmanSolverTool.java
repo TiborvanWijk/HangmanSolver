255e62e6836a91e5b548b5e1bed93f190c431e73
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-11-09 12:37:33 +0100

    optimised suboptimal code + cleanup

--- HangmanSolverTool.java
+++ HangmanSolverTool.java
@@ -1,18 +1,17 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class HangmanSolverTool {
-    public static void main(String[] args) throws IOException {
-        ArrayList<Character> bannedLetters = new ArrayList<>();
-        start(bannedLetters);
+
+    ArrayList<Character> bannedLetters;
+
+    public HangmanSolverTool(ArrayList<Character> bannedLetters) {
+        this.bannedLetters = bannedLetters;
     }
 
-    private static void start(ArrayList<Character> bannedLetters) throws IOException {
+    public void start() {
 
         Map<Integer, Character> lettersAndIndexOfInput;
         Scanner scanner;
@@ -20,27 +19,30 @@ public class HangmanSolverTool {
         scanner = new Scanner(System.in);
 
         System.out.println("Enter the letters that you know and the ones you do not know as: \"_\"");
-        System.out.println("Example: H_ll_");
+        System.out.println("Example: c___ee. For the word \"coffee\" if you knew it has \"c\" and \"e\".");
         confirmedLetters = scanner.nextLine().toUpperCase();
 
         lettersAndIndexOfInput = getLettersAndIndexOfInput(confirmedLetters);
-
         if (confirmedLetters.length() < 2){
             System.out.println("Word needs to be at least two letters long");
-            start(bannedLetters);
+            start();
         }
-
-        printmatchingWords(confirmedLetters, lettersAndIndexOfInput, bannedLetters);
-
+        else {
+            try {
+                printMatchingWords(confirmedLetters, lettersAndIndexOfInput);
+            } catch (IOException e) {
+                throw new RuntimeException("Something went wrong with using the txt file.");
+            }
+        }
         scanner.close();
     }
-    private static void printmatchingWords(String confirmedLetters, Map<Integer, Character> lettersAndIndexOfInput, ArrayList<Character> bannedLetters) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("example.txt"));
+    private void printMatchingWords(String confirmedLetters, Map<Integer, Character> lettersAndIndexOfInput) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Haaima Computers\\Desktop\\dictionary.txt"));
         Map<Character, Integer> totalLettersOfAllWords = new HashMap<>();
 
         for (String currentWord = reader.readLine(); currentWord != null; currentWord = reader.readLine()){
             if (currentWord.equals(confirmedLetters) || !confirmedLetters.contains("_")){
-                System.out.println("Word is already solved");
+                System.out.println("Your word has been solved!!");
                 System.exit(1);
             }
             else if (lettersAndIndexOfInput.size() > 0){
@@ -57,33 +59,31 @@ public class HangmanSolverTool {
         }
 
 
-        mostLetters(totalLettersOfAllWords, bannedLetters, confirmedLetters, lettersAndIndexOfInput);
+        mostLetters(totalLettersOfAllWords, confirmedLetters, lettersAndIndexOfInput);
         reader.close();
     }
 
 
-    private static Map<Integer, Character> getLettersAndIndexOfInput(String confirmedLetters){
+    private Map<Integer, Character> getLettersAndIndexOfInput(String confirmedLetters){
         Map<Integer, Character> lettersAndIndexOfInput = new HashMap<>();
         for (int i = 0; i < confirmedLetters.length(); i++){
             if (confirmedLetters.charAt(i) != '_'){
                 lettersAndIndexOfInput.put(i, confirmedLetters.charAt(i));
             }
         }
-//        System.out.println(lettersAndIndexOfInput);
         return lettersAndIndexOfInput;
     }
-    private static boolean matchesWordsWithLength(String currentWord, String confirmedLetters, Map<Character, Integer> totalLettersOfAllWords){
-        ArrayList<Character> addedLetters = new ArrayList<>();
+    private boolean matchesWordsWithLength(String currentWord, String confirmedLetters, Map<Character, Integer> totalLettersOfAllWords){
 
         if (currentWord.length() != confirmedLetters.length()){
             return false;
         }
 
-        addLettersToList(currentWord, confirmedLetters, totalLettersOfAllWords, addedLetters);
+        addLettersToList(currentWord, confirmedLetters, totalLettersOfAllWords);
         return true;
     }
-    private static void addLettersToList(String currentWord, String confirmedLetters, Map<Character, Integer> totalLettersOfAllWords,
-                                         ArrayList<Character> addedLetters){
+    private void addLettersToList(String currentWord, String confirmedLetters, Map<Character, Integer> totalLettersOfAllWords){
+        Set<Character> addedLetters = new HashSet<>();
         for (int i = 0; i < currentWord.length(); i++){
 
             int count = totalLettersOfAllWords.getOrDefault(currentWord.charAt(i), 0);
@@ -96,10 +96,10 @@ public class HangmanSolverTool {
             }
         }
     }
-    private static boolean matchesLettersAtIndex(String currentWord, Map<Integer, Character> lettersAndIndexOfInput, String confirmedLetters,
+    private boolean matchesLettersAtIndex(String currentWord, Map<Integer, Character> lettersAndIndexOfInput, String confirmedLetters,
                                                  Map<Character, Integer> totalLettersOfAllWords) {
 
-        ArrayList<Character> addedLetters = new ArrayList<>();
+
         for (Map.Entry<Integer, Character> entry : lettersAndIndexOfInput.entrySet()){
             int index = entry.getKey();
             char letter = entry.getValue();
@@ -109,10 +109,10 @@ public class HangmanSolverTool {
             }
         }
 
-        addLettersToList(currentWord, confirmedLetters, totalLettersOfAllWords, addedLetters);
+        addLettersToList(currentWord, confirmedLetters, totalLettersOfAllWords);
         return true;
     }
-    private static void mostLetters(Map<Character, Integer> totalLettersOfAllWords, ArrayList<Character> bannedLetters, String confirmedLetters,
+    private void mostLetters(Map<Character, Integer> totalLettersOfAllWords, String confirmedLetters,
                                     Map<Integer, Character> lettersAndIndexOfInput) throws IOException {
         if (totalLettersOfAllWords.size() == 0){
             System.out.println("This word does not exist.");
@@ -138,33 +138,32 @@ public class HangmanSolverTool {
             }
 
 
-            feedbackOnComputesGuess(totalLettersOfAllWords,mostLetter,confirmedLetters,scanner,bannedLetters,lettersAndIndexOfInput);
+            feedbackOnComputerGuess(totalLettersOfAllWords,mostLetter,confirmedLetters,scanner,lettersAndIndexOfInput);
             scanner.close();
         }
 
     }
-    private static void feedbackOnComputesGuess(Map<Character, Integer> totalLettersOfAllWords, char mostLetter, String confirmedLetters,
-                                                Scanner scanner, ArrayList<Character> bannedLetters, Map<Integer, Character> lettersAndIndexOfInput) throws IOException {
+    private void feedbackOnComputerGuess(Map<Character, Integer> totalLettersOfAllWords, char mostLetter, String confirmedLetters,
+                                         Scanner scanner, Map<Integer, Character> lettersAndIndexOfInput) throws IOException {
 
         System.out.println("Mathematically speaking " + mostLetter + " is the best next guess");
         System.out.println("Was this guess correct? (Y/N)");
         String answer = scanner.nextLine().toUpperCase();
         if (answer.equals("Y")){
-            newInput(confirmedLetters, lettersAndIndexOfInput, totalLettersOfAllWords, mostLetter, bannedLetters);
+            newInput(confirmedLetters, lettersAndIndexOfInput, totalLettersOfAllWords, mostLetter);
         }
         else if(answer.equals("N")){
             totalLettersOfAllWords.remove(mostLetter);
             bannedLetters.add(mostLetter);
-            mostLetters(totalLettersOfAllWords, bannedLetters, confirmedLetters, lettersAndIndexOfInput);
+            mostLetters(totalLettersOfAllWords, confirmedLetters, lettersAndIndexOfInput);
         }
         else {
-            mostLetters(totalLettersOfAllWords, bannedLetters, confirmedLetters, lettersAndIndexOfInput);
+            mostLetters(totalLettersOfAllWords, confirmedLetters, lettersAndIndexOfInput);
         }
     }
 
-    private static void newInput(String confirmedLetters, Map<Integer, Character> lettersAndIndexOfInput,
-                                               Map<Character, Integer> totalLettersOfAllWords, char mostLetter,
-                                               ArrayList<Character> bannedLetters) throws IOException {
+    private void newInput(String confirmedLetters, Map<Integer, Character> lettersAndIndexOfInput,
+                                 Map<Character, Integer> totalLettersOfAllWords, char mostLetter) throws IOException {
         String newInput;
 
         do {
@@ -176,17 +175,16 @@ public class HangmanSolverTool {
         }while (!inputRequirements(newInput, lettersAndIndexOfInput, confirmedLetters, totalLettersOfAllWords, mostLetter));
         lettersAndIndexOfInput = getLettersAndIndexOfInput(newInput);
 
-        printmatchingWords(newInput, lettersAndIndexOfInput, bannedLetters);
+        printMatchingWords(newInput, lettersAndIndexOfInput);
     }
-    private static boolean inputRequirements(String input, Map<Integer, Character> lettersAndIndexOfInput,
-                                             String confirmedLetters, Map<Character, Integer> totalLettersOfAllWords, char mostletter){
-        String mostletterString = String.valueOf(mostletter);
-
+    private boolean inputRequirements(String input, Map<Integer, Character> lettersAndIndexOfInput,
+                                             String confirmedLetters, Map<Character, Integer> totalLettersOfAllWords, char mostFrequentLetter){
+        String mostletterString = String.valueOf(mostFrequentLetter);
 
 
         for (int i = 0; i < input.length(); i++){
             if (!lettersAndIndexOfInput.containsKey(i)){
-                if (input.charAt(i) != '_' && input.charAt(i) != mostletter){
+                if (input.charAt(i) != '_' && input.charAt(i) != mostFrequentLetter){
                     return false;
                 }
             }
---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        HangmanSolverTool tool = new HangmanSolverTool(new ArrayList<>());
+        tool.start();
+    }
+}
